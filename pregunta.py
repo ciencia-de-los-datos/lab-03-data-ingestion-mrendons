0e25a5682b959a85a524cc519b4a750dbb40cb1b
mrendons <mrendons@unal.edu.co>
2024-04-30 22:52:41 -0500

    Intento 4

--- pregunta.py
+++ pregunta.py
@@ -18,24 +18,23 @@ def ingest_data():
         next(file)  # Saltar la segunda línea
 
         for line in file:
-            columns = line.split()
-            if len(columns) >= 1:
+            columns = line.split() # Separar por espacios
+            if len(columns) >= 1: # Si hay al menos un elemento
                 try:
-                    cluster = int(columns[0])
-                    cantidad_de_palabras_clave = int(columns[1])
-                    porcentaje_de_palabras_clave = float(columns[2].replace(',', '.'))
-                    principales_palabras_clave = ' '.join(columns[4:])
-                    data.append([cluster, cantidad_de_palabras_clave, porcentaje_de_palabras_clave, principales_palabras_clave])
+                    cluster = int(columns[0])   # Convertir a entero
+                    cantidad_de_palabras_clave = int(columns[1]) # Convertir a entero
+                    porcentaje_de_palabras_clave = float(columns[2].replace(',', '.')) # Convertir a float
+                    principales_palabras_clave = ' '.join(columns[4:])  # Unir las palabras clave
+                    data.append([cluster, cantidad_de_palabras_clave, porcentaje_de_palabras_clave, principales_palabras_clave]) # Agregar a la lista
                 except ValueError:
                     # Si hay error al convertir a int o float, asumimos que es una línea adicional de palabras clave
                     if len(data) > 0:
-                        palabras_clave = ' '.join(columns)
-                        palabras_clave = palabras_clave.replace('.', '').replace(',', ', ')
-                        data[-1][-1] += ', ' + palabras_clave
+                        principales_palabras_clave = ' '.join(columns) # Unir las palabras clave
+                        data[-1][-1] += ' ' + principales_palabras_clave # Agregar a la última fila
 
-    headers = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
-    df = pd.DataFrame(data, columns=headers)
-    df.columns = df.columns.str.replace(' ', '_').str.lower()
+    headers = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'] # Encabezados
+    df = pd.DataFrame(data, columns=headers) # Crear DataFrame
+    df.columns = df.columns.str.replace(' ', '_').str.lower()  # Reemplazar espacios por guiones bajos y convertir a minúsculas
     return df
 
 # Test
